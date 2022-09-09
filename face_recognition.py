91f7a8d6fa0dedf67db02f96e1791c585f68dcf3
rohit888866 <79111576+rohit888866@users.noreply.github.com>
2022-09-09 12:41:16 +0530

    Update face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -10,9 +10,9 @@ cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath);
 
 font = cv2.FONT_HERSHEY_SIMPLEX
-
-cam = cv2.VideoCapture(0)
-
+gstreamer_str = "sudo gst-launch-1.0 rtspsrc location=rtsp://192.168.1.55:8080/h264_ulaw.sdp latency=10 ! queue ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! videoscale ! appsink"
+cam = cv2.VideoCapture(gstreamer_str, cv2.CAP_GSTREAMER)
+yz=0
 while True:
     
     ret, im =cam.read()
@@ -42,6 +42,9 @@ while True:
         cv2.putText(im, str(Id1), (x,y-40), font, 2, (255,255,255), 3)
 
     cv2.imshow('im',im) 
+    yt= "LTS/"
+    yz=yz+1
+    cv2.imwrite(yt+str(yz)+".jpg",image)
 
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
